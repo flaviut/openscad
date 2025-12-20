d7a8b62c80fe77c29fb3ef5deeb40cd3faf5722d
Flaviu Tamas <me@flaviutamas.com>
2025-12-20 18:55:07 -0500

    chore: enable ccache if installed
    We don't want to edit developers' home directory without
    their permission, but presumably if their system has
    ccache installed, they intend for it to be used.

--- scripts/nix/shell.nix
+++ scripts/nix/shell.nix
@@ -56,6 +56,13 @@ pkgs.mkShell {
     clang-tools
   ];
 
+  shellHook = ''
+    # if the user already has ccache installed, presumably they intend it to be used:
+    if command -v ccache >/dev/null; then
+      export CMAKE_C_COMPILER_LAUNCHER=ccache CMAKE_CXX_COMPILER_LAUNCHER=ccache
+    fi
+  '';
+
   # avoid segfault when showing a file dialog or color picker
   # this is usually handled by GTK wrappers during package build
   GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
